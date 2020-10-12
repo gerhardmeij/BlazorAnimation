e2855f49ad694771fdac516d633fdebc95b989af
Gerhard Meij <gerhardmeij@gmail.com>
2020-10-12 16:31:40 +0200

    Part1

---
+++ FlappyBirdV1/Models/BirdModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlappyBirdV1.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+
+        }
+    }
+}
